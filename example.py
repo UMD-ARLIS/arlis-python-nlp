3838fd12a37d9bc3517888f6587ec2bb4bf5acaa
ayusaxena12 <106680313+ayusaxena12@users.noreply.github.com>
2022-07-19 10:40:31 -0400

    Update example.py

--- example.py
+++ example.py
@@ -2,9 +2,11 @@
 
 from arlis-python-nlp import ArticleSummary
 from arlis-python-nlp import ArticleExtraction
+from arlis-python-nlp import ArticleCategory
 import nltk
 nltk.download('punkt')
 
+#Article Summary's object here is called article
 article = ArticleSummary('https://www.washingtonpost.com/graphics/2019/local/teen-graffiti-hate-crime-divides-maryland-high-school/')
 article.download()
 
@@ -14,7 +16,7 @@ article.nlp()
 article.keywords
 
 #Print article text
-print(article.text)
+articletext = print(article.text)
 
 #Get Summary of article (Full Description on AIID)
 print(article.summary)
@@ -24,3 +26,7 @@ Article.authors
 
 #Get publish date
 article.publish_date
+
+
+#Article Extraction's object here is called extraction
+extraction = ArticleExtraction
