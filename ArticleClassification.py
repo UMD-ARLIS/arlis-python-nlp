00a5629b9156b20b7421756289b70c6cade95c0d
ayusaxena12 <106680313+ayusaxena12@users.noreply.github.com>
2022-07-20 10:18:21 -0400

    Create ArticleClassification.py

---
+++ ArticleClassification.py
@@ -0,0 +1,5 @@
+#https://huggingface.co/joeddav/xlm-roberta-large-xnli?text=Can+you+please+amend+the+invoice+to+reflect+true+capital+expenditure+and+anticipated+revenue%3F&candidate_labels=Medical%2Cbusiness%2Cfinance&multi_class=true
+
+
+import transformers
+from transformers import pipeline
